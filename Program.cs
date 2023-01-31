5fd55c454b5be27fc284b6b62457bea9d65160ab
rakifsherifov <122780002+rakifsherifov@users.noreply.github.com>
2023-01-31 22:23:48 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Find1stWord("hello usaqlarr");
+            Console.WriteLine(word);
+        }
+        static int Find1stWord(string text)
+        {
+            string word = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                    break;
+               do
+                {
+                }
+                while(text.Length > 0);
+                    
+                   
+                
+
+
+
+
+
+            }
+            return word.Length;
+            //yaza bilmedim muellim, alinmadi
+
+        }
+    }
+}
