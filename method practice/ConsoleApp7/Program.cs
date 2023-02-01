9810d2c48e401c1fa16f625071b56a39fc96c464
salto <rakif2121@gmail.com>
2023-01-31 23:07:36 -0800

    method

---
+++ method practice/ConsoleApp7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    //Verilmiş yazının əvvəlindəki boşluqları silib qayataran metod
+    {
+        static void Main(string[] args)
+        {
+            var output = RemoveSpace ("Hello usaqlar");
+            Console.WriteLine(output);
+        }
+        static string RemoveSpace(string input)
+        {
+            string output= "";
+
+            for (int i = 0; i <input.Length; i++)
+            {
+                if (input[i] !=' ')
+                {
+                    output += input[i];
+                }
+                
+            }
+            return output;
+        }
+    }
+}
